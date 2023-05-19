f171fc8a638ea6ed6ed9b07b72fd73522805fc9b
Sasha <646415@student.inholland.nl>
2023-05-19 15:54:08 +0200

    pagination, sanitation, request body format

--- App/Controllers/FacilityController.php
+++ App/Controllers/FacilityController.php
@@ -9,14 +9,24 @@ use App\Plugins\Http\Exceptions;
 class FacilityController extends BaseController
 {
     /**
-     * @return object|null
+     * Create a new facility.
+     *
+     * HTTP Method: POST
+     * URL: /facilities
+     *
+     * @return object
      */
     public function createOne(): ?object
     {
-        $data = json_decode(file_get_contents('php://input'));
+        $data = $this->getRequestData();
 
         $sql = "INSERT INTO facilities (name, created_at, tag_id, location_id) VALUES (?, ?, ?, ?)";
-        $result = $this->db->executeQuery($sql, [$data->name ?? null, $data->created_at ?? null, $data->tag_id ?? null, $data->location_id ?? null]);
+        $result = $this->db->executeQuery($sql, [
+            $data->name ?? null,
+            $data->created_at ?? null,
+            $data->tag_id ?? null,
+            $data->location_id ?? null
+        ]);
 
         $status = new Status\Ok(['Object created successfully' => $result]);
         $status->send();
@@ -25,18 +35,27 @@ class FacilityController extends BaseController
     }
 
     /**
+     * Get a facility by ID.
+     *
+     * HTTP Method: GET
+     * URL: /facilities/{id}
+     *
      * @param int $id
      * @return object
      */
     public function readOne(int $id): object
     {
         $sql = "SELECT f.id, f.name, f.created_at, l.city, l.address, l.zip_code, l.country_code, l.phone_number, t.name AS tag_name
-        FROM facilities AS f
-        JOIN locations AS l ON f.location_id = l.id
-        LEFT JOIN tags AS t ON f.tag_id = t.id
-        WHERE f.id = ?";
+                    FROM facilities AS f
+                    JOIN locations AS l ON f.location_id = l.id
+                    LEFT JOIN tags AS t ON f.tag_id = t.id
+                    WHERE f.id = ?";
 
         $result = $this->db->executeQuery2($sql, [$id]);
+
+        if (empty($result)) {
+            throw new Exceptions\NotFound('Facility not found');
+        }
 
         $status = new Status\Ok(['Object' => $result]);
         $status->send();
@@ -45,7 +64,12 @@ class FacilityController extends BaseController
     }
 
     /**
-     * @return object 
+     * Get all facilities.
+     *
+     * HTTP Method: GET
+     * URL: /facilities
+     *
+     * @return object
      */
     public function readAll(): object
     {
@@ -57,6 +81,11 @@ class FacilityController extends BaseController
                     JOIN locations AS l ON f.location_id = l.id
                     LEFT JOIN tags AS t ON f.tag_id = t.id";
 
+        $page = $_GET['page'] ?? 1;
+        $perPage = $_GET['per_page'] ?? 5;
+        $offset = ($page - 1) * $perPage;
+        $sql .= " LIMIT $offset, $perPage";
+
         $result = $this->db->executeQuery2($sql, $bind);
         $response["data"] =  $result;
 
@@ -67,15 +96,30 @@ class FacilityController extends BaseController
     }
 
     /**
+     * Update a facility by ID.
+     *
+     * HTTP Method: PUT
+     * URL: /facilities/{id}
+     *
      * @param int $id
      * @return object
      */
     public function updateOne(int $id): object
     {
-        $data = json_decode(file_get_contents('php://input'));
+        $data = $this->getRequestData();
 
         $sql = "UPDATE facilities SET name = ?, created_at = ?, tag_id = ?, location_id = ? WHERE id = ?";
-        $result = $this->db->executeQuery($sql, [$data->name ?? null, $data->created_at ?? null, $data->tag_id ?? null, $data->location_id ?? null, $id]);
+        $result = $this->db->executeQuery($sql, [
+            $data->name ?? null,
+            $data->created_at ?? null,
+            $data->tag_id ?? null,
+            $data->location_id ?? null,
+            $id
+        ]);
+
+        if ($result === 0) {
+            throw new Exceptions\NotFound('Facility not found');
+        }
 
         $status = new Status\Ok(['Object updated successfully' => $result]);
         $status->send();
@@ -84,18 +128,68 @@ class FacilityController extends BaseController
     }
 
     /**
+     * Delete a facility by ID.
+     *
+     * HTTP Method: DELETE
+     * URL: /facilities/{id}
+     *
      * @param int $id
      * @return object
      */
     public function deleteOne(int $id): object
     {
         $sql = "DELETE FROM facilities WHERE id = ?";
-
         $result = $this->db->executeQuery($sql, [$id]);
+
+        if ($result === 0) {
+            throw new Exceptions\NotFound('Facility not found');
+        }
 
         $status = new Status\Ok(['Object deleted successfully' => $result]);
         $status->send();
 
         return $status;
+    }
+
+    /**
+     * Get the request data based on the content type and sanitize it.
+     *
+     * @return object
+     */
+    private function getRequestData(): object
+    {
+        $contentType = $_SERVER['CONTENT_TYPE'] ?? '';
+        $requestData = (object)[];
+
+        if ($contentType === 'application/json') {
+            $requestData = (object)json_decode(file_get_contents('php://input'), true);
+        } elseif ($contentType === 'application/x-www-form-urlencoded') {
+            parse_str(file_get_contents('php://input'), $requestData);
+            $requestData = (object)$requestData;
+        } elseif ($contentType === 'multipart/form-data') {
+            $requestData = (object)($_POST + $_FILES);
+        }
+        $sanitizedData = $this->sanitize($requestData);
+
+        return $sanitizedData;
+    }
+
+    /**
+     * Sanitize a value using htmlspecialchars.
+     *
+     * @param mixed $value
+     * @return mixed
+     */
+    private function sanitize($value)
+    {
+        if (is_array($value)) {
+            return array_map([$this, 'sanitize'], $value);
+        }
+
+        if (is_string($value)) {
+            return htmlspecialchars($value, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+        }
+
+        return $value;
     }
 }
