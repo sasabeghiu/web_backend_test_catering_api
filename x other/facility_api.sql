56307e7f4c4edc5e93c33983329e4d18b5ef6a7e
Sasha <646415@student.inholland.nl>
2023-05-20 17:36:09 +0200

    added db

---
+++ x other/facility_api.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2023 at 05:35 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `facility api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facilities`
+--
+
+CREATE TABLE `facilities` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `location_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `facilities`
+--
+
+INSERT INTO `facilities` (`id`, `name`, `created_at`, `location_id`) VALUES
+(82, 'School Facility', '2023-05-20 14:20:00', 37),
+(83, 'Work Facility', '2023-05-20 14:20:00', 36),
+(84, 'Work Facility', '2023-05-20 14:20:00', 37),
+(85, 'School Facility', '2023-05-20 14:20:00', 37),
+(86, 'School Facility', '2023-05-20 14:20:00', 38),
+(87, 'Test Facility', '2023-05-20 14:20:00', 38),
+(88, 'School Facility', '2023-05-20 14:20:00', 38);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facility_tags`
+--
+
+CREATE TABLE `facility_tags` (
+  `facility_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `facility_tags`
+--
+
+INSERT INTO `facility_tags` (`facility_id`, `tag_id`) VALUES
+(82, 13),
+(82, 14),
+(83, 15),
+(83, 17),
+(84, 14),
+(84, 15),
+(85, 14),
+(85, 16);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locations`
+--
+
+CREATE TABLE `locations` (
+  `id` int(11) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `zip_code` varchar(255) NOT NULL,
+  `country_code` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `locations`
+--
+
+INSERT INTO `locations` (`id`, `city`, `address`, `zip_code`, `country_code`, `phone_number`) VALUES
+(36, 'Haarlem', 'Schoonzichtlaan', '2015 CL', 'NL', '0654762756'),
+(37, 'Amsterdam', 'Rijkplaan', '2045 LL', 'NL', '0654762156'),
+(38, 'Amsterdam', 'Museumplein', '1045 XX', 'NL', '0664762156');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`id`, `name`) VALUES
+(14, 'AMS'),
+(17, 'HAA'),
+(16, 'SCHOOL'),
+(13, 'TEST'),
+(15, 'WORK'),
+(18, 'XXX');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `facilities`
+--
+ALTER TABLE `facilities`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `location_id` (`location_id`);
+
+--
+-- Indexes for table `facility_tags`
+--
+ALTER TABLE `facility_tags`
+  ADD PRIMARY KEY (`facility_id`,`tag_id`),
+  ADD KEY `facility_id` (`facility_id`),
+  ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Indexes for table `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `facilities`
+--
+ALTER TABLE `facilities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
+
+--
+-- AUTO_INCREMENT for table `locations`
+--
+ALTER TABLE `locations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `facilities`
+--
+ALTER TABLE `facilities`
+  ADD CONSTRAINT `fk_facility_location` FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`);
+
+--
+-- Constraints for table `facility_tags`
+--
+ALTER TABLE `facility_tags`
+  ADD CONSTRAINT `fk_facility_id` FOREIGN KEY (`facility_id`) REFERENCES `facilities` (`id`),
+  ADD CONSTRAINT `fk_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
